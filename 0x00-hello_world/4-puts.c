c5b13103a9bedd1371ac8c0740ce3b1309e19456
theophilusifeanyi <theophilusifeanyi82@gmail.com>
2022-10-19 13:54:40 -0700

    my alx course

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - puts the string in the put function
+ * Description: print a string using followed by a new line
+ * this program puts "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
