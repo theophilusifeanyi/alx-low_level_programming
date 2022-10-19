91c8c2ed39095284e80cecf0b0c7c7d79dfa575b
theophilusifeanyi <theophilusifeanyi82@gmail.com>
2022-10-19 14:35:30 -0700

    i love alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+
+/**
+ * main	- puts string in the printf function
+ *
+ * Description:	print a string using followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
